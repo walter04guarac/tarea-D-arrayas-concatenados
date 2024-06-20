7ccc4d27df5d1119fa08902ffeac9074057ab5c8
walter04guarac <130495513+walter04guarac@users.noreply.github.com>
2024-06-19 20:10:59 -0500

    .

---
+++ conctElemtArray.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let i;
+let j;
+let result;
+
+let array=[0,2,4,8];
+
+for(i=0; i<array.length; i++){
+    
+    for(j=0; j<array.length; j++){
+       let one=array[i];
+       let two=array[j];
+
+        alert(one +""+ two);
+    }
+}
---
+++ sizeArray.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let nums = [7, 4, 0, 9, 7, 6, 3, 8];
+let contador = 0; 
+
+for (let i = 0; i < nums.length; i++) {
+    contador++; 
+}
+
+alert("El tamaño del array es: " + contador);
